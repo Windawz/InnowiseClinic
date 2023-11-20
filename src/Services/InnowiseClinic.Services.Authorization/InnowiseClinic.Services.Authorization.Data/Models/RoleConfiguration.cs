467124545d6a915bd37c3393a8fba191001acdf0
Windawz <46968683+Windawz@users.noreply.github.com>
2023-11-20 15:18:11 +0300

    Ensure role name case-insensitivity in data layer

---
+++ src/Services/InnowiseClinic.Services.Authorization/InnowiseClinic.Services.Authorization.Data/Models/RoleConfiguration.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace InnowiseClinic.Services.Authorization.Data.Models;
+
+/// <summary>
+/// Configuration for the <see cref="Role"/> entity.
+/// </summary>
+internal class RoleConfiguration : IEntityTypeConfiguration<Role>
+{
+    /// <summary>
+    /// Configures the <see cref="Role"/> entity.
+    /// </summary>
+    /// <param name="builder"><inheritdoc/></param>
+    public void Configure(EntityTypeBuilder<Role> builder)
+    {
+        // Ensure case-insensitivity.
+        builder.Property(role => role.Name)
+            .UseCollation("Latin1_General_CI_AS");
+    }
+}
