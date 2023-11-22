d31824489396128e353df1fdbab137e3b7c163c3
Windawz <46968683+Windawz@users.noreply.github.com>
2023-11-22 17:06:56 +0300

    Rename the erroneously named IRoleFinder to IRoleResolver

--- src/Services/InnowiseClinic.Services.Authorization/InnowiseClinic.Services.Authorization.Services/IRoleResolver.cs
+++ src/Services/InnowiseClinic.Services.Authorization/InnowiseClinic.Services.Authorization.Services/IRoleResolver.cs
@@ -5,7 +5,7 @@ namespace InnowiseClinic.Services.Authorization.Services;
 /// <summary>
 /// Searches for roles by various criteria.
 /// </summary>
-public interface IRoleFinder
+public interface IRoleResolver
 {
     /// <summary>
     /// Searches for a role with a specific name.
