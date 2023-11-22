75624c41284312cd778072c4690f420493773531
Windawz <46968683+Windawz@users.noreply.github.com>
2023-11-22 16:39:30 +0300

    Add ID to the service Role model

--- src/Services/InnowiseClinic.Services.Authorization/InnowiseClinic.Services.Authorization.Services/Models/Role.cs
+++ src/Services/InnowiseClinic.Services.Authorization/InnowiseClinic.Services.Authorization.Services/Models/Role.cs
@@ -3,5 +3,6 @@ using System.Collections.Immutable;
 namespace InnowiseClinic.Services.Authorization.Services.Models;
 
 public record Role(
+    int Id,
     string Name,
     IImmutableSet<Role> RegisterableRoles);
