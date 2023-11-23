7e9311047f1bb69453b27ee236e248bb79de7de0
Windawz <46968683+Windawz@users.noreply.github.com>
2023-11-23 15:01:07 +0300

    Implement an account resolver

---
+++ src/Services/InnowiseClinic.Services.Authorization/InnowiseClinic.Services.Authorization.Services/AccountResolver.cs
@@ -0,0 +1,53 @@
+using InnowiseClinic.Services.Authorization.Data;
+using InnowiseClinic.Services.Authorization.Services.Models;
+
+namespace InnowiseClinic.Services.Authorization.Services;
+
+/// <summary>
+/// A default implementation of <see cref="IAccountResolver"/>.
+/// </summary>
+public class AccountResolver : IAccountResolver
+{
+    private readonly AuthorizationDbContext _dbContext;
+
+    /// <summary>
+    /// Creates an instance of <see cref="AccountResolver"/>.
+    /// </summary>
+    /// <param name="dbContext">An instance of <see cref="AuthorizationDbContext"/>.</param>
+    public AccountResolver(AuthorizationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    /// <inheritdoc/>
+    public Account? ResolveByEmail(string email)
+    {
+        var dataAccount = _dbContext.Accounts
+            .FirstOrDefault(account => account.Email == email);
+
+        if (dataAccount is not null)
+        {
+            return Mapping.FromDataAccount(dataAccount);
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    /// <inheritdoc/>
+    public Account? ResolveById(int id)
+    {
+        var dataAccount = _dbContext.Accounts
+            .SingleOrDefault(account => account.Id == id);
+
+        if (dataAccount is not null)
+        {
+            return Mapping.FromDataAccount(dataAccount);
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
