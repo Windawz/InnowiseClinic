46ad77e1dc514392d1bea61788fb124e4bc46f1e
Windawz <46968683+Windawz@users.noreply.github.com>
2023-11-23 15:34:08 +0300

    Alter account registration interface and get rid of the registration info model

--- src/Services/InnowiseClinic.Services.Authorization/InnowiseClinic.Services.Authorization.Services/IAccountRegistrator.cs
+++ src/Services/InnowiseClinic.Services.Authorization/InnowiseClinic.Services.Authorization.Services/IAccountRegistrator.cs
@@ -10,18 +10,21 @@ public interface IAccountRegistrator
     /// <summary>
     /// Registers an account on behalf of said account.
     /// </summary>
-    /// <param name="info">An instance of <see cref="RegistrationInfo"/></param>
+    /// <param name="email">Account email.</param>
+    /// <param name="password">Account password in plain text.</param>
     /// <returns>
     /// The registered account.
     /// </returns>
-    Account RegisterSelf(RegistrationInfo info);
+    Account RegisterSelf(string email, string password);
     /// <summary>
     /// Registers an account on behalf of <paramref name="initiator"/>.
     /// </summary>
-    /// <param name="info">An instance of <see cref="RegistrationInfo"/></param>
     /// <param name="initiator">The account that is responsible for registering this account.</param>
+    /// <param name="email">Account email.</param>
+    /// <param name="password">Account password in plain text.</param>
+    /// <param name="roles">Account roles.</param>
     /// <returns>
     /// The registered account.
     /// </returns>
-    Account RegisterOther(RegistrationInfo info, Account initiator);
+    Account RegisterOther(Account initiator, string email, string password, IReadOnlyCollection<Role> roles);
 }
