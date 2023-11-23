778e93dec687a25749260ccb39a66b72f67ed346
Windawz <46968683+Windawz@users.noreply.github.com>
2023-11-23 11:30:21 +0300

    Add documentation to RoleResolver

--- src/Services/InnowiseClinic.Services.Authorization/InnowiseClinic.Services.Authorization.Services/RoleResolver.cs
+++ src/Services/InnowiseClinic.Services.Authorization/InnowiseClinic.Services.Authorization.Services/RoleResolver.cs
@@ -4,15 +4,23 @@ using Microsoft.EntityFrameworkCore;
 
 namespace InnowiseClinic.Services.Authorization.Services;
 
+/// <summary>
+/// A default implementation of <see cref="IRoleResolver"/>.
+/// </summary>
 public class RoleResolver : IRoleResolver
 {
     private readonly AuthorizationDbContext _dbContext;
 
+    /// <summary>
+    /// Creates an instance of <see cref="RoleResolver"/>.
+    /// </summary>
+    /// <param name="dbContext">An instance of <see cref="AuthorizationDbContext"/>.</param>
     public RoleResolver(AuthorizationDbContext dbContext)
     {
         _dbContext = dbContext;
     }
 
+    /// <inheritdoc/>
     public Role? ResolveByName(string name)
     {
         var dataRole = _dbContext.Roles
