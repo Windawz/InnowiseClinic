1c0097e8a0a1f9bb2e856f1ae71b25d6865ab955
Windawz <46968683+Windawz@users.noreply.github.com>
2023-12-07 16:36:46 +0300

    Configure JWT bearer auth

--- src/Microservices/Authorization/Api/Program.cs
+++ src/Microservices/Authorization/Api/Program.cs
@@ -1,4 +1,7 @@
 
+using InnowiseClinic.Microservices.Authorization.Api.Configuration.Configurators.Auth;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 namespace InnowiseClinic.Microservices.Authorization.Api;
 
 public class Program
@@ -7,16 +10,16 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-
         builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer();
+
+        builder.Services.ConfigureOptions<ConfigureJwtBearerOptions>();
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
@@ -24,10 +27,8 @@ public class Program
         }
 
         app.UseHttpsRedirection();
-
+        app.UseAuthentication();
         app.UseAuthorization();
-
-
         app.MapControllers();
 
         app.Run();
