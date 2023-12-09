e7bcde2dfe8bc590f7d35aed73d9a106d546561e
Windawz <46968683+Windawz@users.noreply.github.com>
2023-12-10 01:43:42 +0300

    Disable automatic database id generation for refresh tokens

---
+++ src/Microservices/Authorization/Data/Entities/Configurations/RefreshTokenConfiguration.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace InnowiseClinic.Microservices.Authorization.Data.Entities.Configurations;
+
+public class RefreshTokenConfiguration : IEntityTypeConfiguration<RefreshTokenEntity>
+{
+    public void Configure(EntityTypeBuilder<RefreshTokenEntity> builder)
+    {
+        builder.Property(entity => entity.Id)
+            .ValueGeneratedNever();
+    }
+}
