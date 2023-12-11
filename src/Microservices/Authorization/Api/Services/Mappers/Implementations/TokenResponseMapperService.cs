2d240b909ea313b377010bb6303ebd36a0ede6c1
Windawz <46968683+Windawz@users.noreply.github.com>
2023-12-11 16:25:15 +0300

    Implement ITokenResponseMapperService

---
+++ src/Microservices/Authorization/Api/Services/Mappers/Implementations/TokenResponseMapperService.cs
@@ -0,0 +1,21 @@
+using InnowiseClinic.Microservices.Authorization.Api.DataTransferObjects.Responses;
+using InnowiseClinic.Microservices.Authorization.Api.Services.Mappers.Interfaces;
+using InnowiseClinic.Microservices.Authorization.Application.Models;
+
+namespace InnowiseClinic.Microservices.Authorization.Api.Services.Mappers.Implementations;
+
+public class TokenResponseMapperService(IRefreshTokenStringMapperService refreshTokenStringMapperService) : ITokenResponseMapperService
+{
+    private readonly IRefreshTokenStringMapperService _refreshTokenStringMapperService = refreshTokenStringMapperService;
+
+    public TokenResponse MapFromTokenPair(AccessToken accessToken, RefreshToken refreshToken)
+    {
+        return new()
+        {
+            AccessToken = accessToken.SignedValue,
+            RefreshToken = _refreshTokenStringMapperService.MapFromRefreshToken(refreshToken),
+            TokenType = accessToken.TokenType,
+            ExpiresInSeconds = (int)accessToken.ExpiresAt.Subtract(accessToken.CreatedAt).TotalSeconds,
+        };
+    }
+}
