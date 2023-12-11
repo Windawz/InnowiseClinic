c81e0990be6401a5dd8c3da9c4ea2a884a66b95b
Windawz <46968683+Windawz@users.noreply.github.com>
2023-12-11 16:22:21 +0300

    Implement IRefreshRequestMapperService

---
+++ src/Microservices/Authorization/Api/Services/Mappers/Implementations/RefreshRequestMapperService.cs
@@ -0,0 +1,16 @@
+using InnowiseClinic.Microservices.Authorization.Api.DataTransferObjects.Requests;
+using InnowiseClinic.Microservices.Authorization.Api.Services.Mappers.Interfaces;
+using InnowiseClinic.Microservices.Authorization.Application.Models;
+
+namespace InnowiseClinic.Microservices.Authorization.Api.Services.Mappers.Implementations;
+
+public class RefreshRequestMapperService(IRefreshTokenStringMapperService refreshTokenStringMapperService) : IRefreshRequestMapperService
+{
+    private readonly IRefreshTokenStringMapperService _refreshTokenStringMapperService = refreshTokenStringMapperService;
+
+    public RefreshToken MapFromRefreshRequest(RefreshRequest request)
+    {
+        return _refreshTokenStringMapperService.MapToRefreshToken(
+            request.RefreshToken.Trim());
+    }
+}
