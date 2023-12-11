8a82d7f59aea245d6f81cc5a03f3c2a120b9744c
Windawz <46968683+Windawz@users.noreply.github.com>
2023-12-11 12:12:07 +0300

    Update IAccountService exception documentation

--- src/Microservices/Authorization/Application/Services/Interfaces/IAccountService.cs
+++ src/Microservices/Authorization/Application/Services/Interfaces/IAccountService.cs
@@ -1,12 +1,14 @@
 using InnowiseClinic.Microservices.Authorization.Application.Models;
+using InnowiseClinic.Microservices.Authorization.Application.Services.Exceptions;
 
 namespace InnowiseClinic.Microservices.Authorization.Application.Services.Interfaces;
 
 public interface IAccountService
 {
-    /// <exception cref="Exceptions.AccountNotFoundException">
+    /// <exception cref="AccountNotFoundException"/>
+    /// /// <exception cref="InvalidPasswordException"/>
     Task<Account> AccessAccountAsync(string email, string password);
 
-    /// <exception cref="Exceptions.AccountAlreadyExistsException"/>
+    /// <exception cref="AccountAlreadyExistsException"/>
     Task CreateAccountAsync(string email, string password, string roleName);
 }
