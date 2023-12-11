e8d9a254ed6f965fcdacc300a686c7784a728e35
Windawz <46968683+Windawz@users.noreply.github.com>
2023-12-11 13:46:36 +0300

    Implement ILogInService

---
+++ src/Microservices/Authorization/Api/Services/Implementations/LogInService.cs
@@ -0,0 +1,26 @@
+using InnowiseClinic.Microservices.Authorization.Api.DataTransferObjects.Requests;
+using InnowiseClinic.Microservices.Authorization.Api.DataTransferObjects.Responses;
+using InnowiseClinic.Microservices.Authorization.Api.Services.Interfaces;
+using InnowiseClinic.Microservices.Authorization.Application.Services.Interfaces;
+
+namespace InnowiseClinic.Microservices.Authorization.Api.Services.Implementations;
+
+public class LogInService(
+    IAccountService accountService,
+    IAccessTokenService accessTokenService,
+    IRefreshTokenService refreshTokenService,
+    ILogInResponseMapperService logInResponseMapperService) : ILogInService
+{
+    private readonly IAccountService _accountService = accountService;
+    private readonly IAccessTokenService _accessTokenService = accessTokenService;
+    private readonly IRefreshTokenService _refreshTokenService = refreshTokenService;
+    private readonly ILogInResponseMapperService _logInResponseMapperService = logInResponseMapperService;
+
+    public async Task<LogInResponse> LogInAsync(LogInRequest request)
+    {
+        var account = await _accountService.AccessAccountAsync(request.Email, request.Password);
+        var accessToken = await _accessTokenService.GenerateTokenAsync(account.Role);
+        var refreshToken = await _refreshTokenService.CreateRefreshTokenAsync();
+        return _logInResponseMapperService.MapToLogInResponse(accessToken, refreshToken);
+    }
+}
