972636b3bc01ba52d0ebe01c0dabbb488bdee160
Windawz <46968683+Windawz@users.noreply.github.com>
2023-12-11 16:56:27 +0300

    Add missing exception documentation

--- src/Microservices/Authorization/Application/Services/Interfaces/IRefreshTokenService.cs
+++ src/Microservices/Authorization/Application/Services/Interfaces/IRefreshTokenService.cs
@@ -1,9 +1,12 @@
 using InnowiseClinic.Microservices.Authorization.Application.Models;
+using InnowiseClinic.Microservices.Authorization.Application.Services.Exceptions;
 
 namespace InnowiseClinic.Microservices.Authorization.Application.Services.Interfaces;
 
 public interface IRefreshTokenService
 {
     Task<RefreshToken> CreateRefreshTokenAsync(Role role);
+    
+    /// <exception cref="InvalidRefreshTokenException"/>
     Task<RefreshToken> CreateReplacementRefreshTokenAsync(RefreshToken refreshToken);
 }
--- src/Microservices/Authorization/Application/Services/Mappers/Interfaces/IRoleMapperService.cs
+++ src/Microservices/Authorization/Application/Services/Mappers/Interfaces/IRoleMapperService.cs
@@ -1,9 +1,13 @@
 using InnowiseClinic.Microservices.Authorization.Application.Models;
+using InnowiseClinic.Microservices.Authorization.Application.Services.Exceptions;
 
 namespace InnowiseClinic.Microservices.Authorization.Application.Services.Mappers.Interfaces;
 
 public interface IRoleMapperService
 {
+    /// <exception cref="InvalidOperationException"/>
     string MapToRoleName(Role role);
+
+    /// <exception cref="UnknownRoleException"/>
     Role MapFromRoleName(string roleName);
 }
