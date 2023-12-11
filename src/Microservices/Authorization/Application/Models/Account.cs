862659f67c45f3289ac5d19d68be8119db184af9
Windawz <46968683+Windawz@users.noreply.github.com>
2023-12-11 12:08:52 +0300

    Adjust the account model

--- src/Microservices/Authorization/Application/Models/Account.cs
+++ src/Microservices/Authorization/Application/Models/Account.cs
@@ -3,9 +3,10 @@ namespace InnowiseClinic.Microservices.Authorization.Application.Models;
 public record Account(
     Guid Id,
     string Email,
+    string Password,
     bool IsEmailVerified,
     string? CreatedByEmail,
     DateTime CreatedAt,
     string? UpdatedByEmail,
     DateTime? UpdatedAt,
-    string Role);
+    Role Role);
