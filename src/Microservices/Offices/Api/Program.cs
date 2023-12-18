f9a8e319fb40fc632f8f3e8a32632ba5acb06c9f
Windawz <46968683+Windawz@users.noreply.github.com>
2023-12-18 13:48:12 +0300

    Add initial API setup

--- src/Microservices/Offices/Api/Program.cs
+++ src/Microservices/Offices/Api/Program.cs
@@ -1,3 +1,9 @@
+using FluentValidation;
+using InnowiseClinic.Microservices.Offices.Data.Contexts;
+using InnowiseClinic.Microservices.Shared.Api.ExceptionHandlers;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using SharpGrip.FluentValidation.AutoValidation.Mvc.Extensions;
 
 namespace InnowiseClinic.Microservices.Offices.Api;
 
@@ -10,6 +16,20 @@ public class Program
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer();
+        builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+        builder.Services.AddProblemDetails();
+        builder.Services.AddLogging();
+        builder.Services.AddFluentValidationAutoValidation();
+
+        builder.Services.AddDbContext<OfficesDbContext>(dbContextOptionsBuilder =>
+        {
+            dbContextOptionsBuilder.UseNpgsql(
+                builder.Configuration.GetConnectionString("Default"));
+        });
+
+        ValidatorOptions.Global.LanguageManager.Enabled = false;
 
         var app = builder.Build();
 
@@ -19,7 +39,9 @@ public class Program
             app.UseSwaggerUI();
         }
 
+        app.UseExceptionHandler();
         app.UseHttpsRedirection();
+        app.UseAuthentication();
         app.UseAuthorization();
         app.MapControllers();
 
