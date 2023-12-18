28fa8be3682036245736442d6b0a73cad6be933f
Windawz <46968683+Windawz@users.noreply.github.com>
2023-12-18 17:21:02 +0300

    Move offices DB context to shared code

--- src/Microservices/Offices/Data/Contexts/OfficesDbContext.cs
+++ src/Microservices/Offices/Data/Contexts/OfficesDbContext.cs
@@ -1,9 +1,10 @@
 using System.Reflection;
+using InnowiseClinic.Microservices.Shared.Data.Contexts;
 using Microsoft.EntityFrameworkCore;
 
 namespace InnowiseClinic.Microservices.Offices.Data.Contexts;
 
-public class OfficesDbContext : DbContext
+public class OfficesDbContext : BasicDbContext<OfficesDbContext>
 {
     public OfficesDbContext(DbContextOptions<OfficesDbContext> options) : base(options) { }
 
