68a91ceaf5b163425fe90c8aeaa882d3bfabc558
Windawz <46968683+Windawz@users.noreply.github.com>
2023-12-22 16:00:18 +0300

    Add office DB set accessor to DB context

--- src/Microservices/Offices/Data/Contexts/OfficesDbContext.cs
+++ src/Microservices/Offices/Data/Contexts/OfficesDbContext.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using InnowiseClinic.Microservices.Offices.Data.Entities;
 using InnowiseClinic.Microservices.Shared.Data.Contexts;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,6 +8,9 @@ namespace InnowiseClinic.Microservices.Offices.Data.Contexts;
 public class OfficesDbContext : BasicDbContext<OfficesDbContext>
 {
     public OfficesDbContext(DbContextOptions<OfficesDbContext> options) : base(options) { }
+
+    public DbSet<OfficeEntity> Offices =>
+        Set<OfficeEntity>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
