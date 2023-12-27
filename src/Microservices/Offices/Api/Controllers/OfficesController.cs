8d9d8a98cadc3ec2ffab1f5d989b73cd018501e7
Windawz <46968683+Windawz@users.noreply.github.com>
2023-12-27 11:58:04 +0300

    Use a role name constant in the AuthorizeAttribute on the offices controller

--- src/Microservices/Offices/Api/Controllers/OfficesController.cs
+++ src/Microservices/Offices/Api/Controllers/OfficesController.cs
@@ -1,5 +1,6 @@
 using InnowiseClinic.Microservices.Offices.Api.DataTransferObjects.Requests;
 using InnowiseClinic.Microservices.Offices.Api.DataTransferObjects.Responses;
+using InnowiseClinic.Microservices.Shared.Api.Constants;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,7 +8,7 @@ namespace InnowiseClinic.Microservices.Offices.Api.Controllers;
 
 [ApiController]
 [Route("offices")]
-[Authorize("receptionist")]
+[Authorize(Roles = RoleName.Receptionist)]
 public class OfficesController : ControllerBase
 {
     [HttpGet("{id}")]
