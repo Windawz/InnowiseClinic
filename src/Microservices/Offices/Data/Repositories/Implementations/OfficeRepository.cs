28288abb822e7fc8839e180fddc1df69d30eac72
Windawz <46968683+Windawz@users.noreply.github.com>
2023-12-27 09:20:25 +0300

    Add method to office repository for retrieving pages of offices

--- src/Microservices/Offices/Data/Repositories/Implementations/OfficeRepository.cs
+++ src/Microservices/Offices/Data/Repositories/Implementations/OfficeRepository.cs
@@ -2,10 +2,28 @@ using InnowiseClinic.Microservices.Offices.Data.Contexts;
 using InnowiseClinic.Microservices.Offices.Data.Entities;
 using InnowiseClinic.Microservices.Offices.Data.Repositories.Interfaces;
 using InnowiseClinic.Microservices.Shared.Data.Repositories.Implementations;
+using Microsoft.EntityFrameworkCore;
 
 namespace InnowiseClinic.Microservices.Offices.Data.Repositories.Implementations;
 
 public class OfficeRepository : AsyncRepository<OfficeEntity, OfficesDbContext>, IOfficeRepository
 {
     public OfficeRepository(OfficesDbContext dbContext) : base(dbContext) { }
+
+    public async Task<ICollection<OfficeEntity>> GetPageAsync(int count, Guid? start = null)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(count, nameof(count));
+
+        IQueryable<OfficeEntity> queryable = DbContext.Offices;
+
+        if (start is Guid guid)
+        {
+            queryable = queryable
+                .Where(office => office.Id.CompareTo(guid) > 0);
+        }
+
+        return await queryable
+            .Take(count)
+            .ToListAsync();
+    }
 }
--- src/Microservices/Offices/Data/Repositories/Interfaces/IOfficeRepository.cs
+++ src/Microservices/Offices/Data/Repositories/Interfaces/IOfficeRepository.cs
@@ -3,4 +3,7 @@ using InnowiseClinic.Microservices.Shared.Data.Repositories.Interfaces;
 
 namespace InnowiseClinic.Microservices.Offices.Data.Repositories.Interfaces;
 
-public interface IOfficeRepository : IAsyncRepository<OfficeEntity> { }
+public interface IOfficeRepository : IAsyncRepository<OfficeEntity>
+{
+    Task<ICollection<OfficeEntity>> GetPageAsync(int count, Guid? start = null);
+}
