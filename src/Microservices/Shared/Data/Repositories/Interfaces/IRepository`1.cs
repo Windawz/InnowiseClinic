976670e2621f4c25dbc22a7e06e40b183edc456a
Windawz <46968683+Windawz@users.noreply.github.com>
2023-12-28 12:22:50 +0300

    Rename IAsyncRepository/AsyncRepository to IRepository/Repository

--- src/Microservices/Shared/Data/Repositories/Interfaces/IRepository`1.cs
+++ src/Microservices/Shared/Data/Repositories/Interfaces/IRepository`1.cs
@@ -2,7 +2,7 @@ using InnowiseClinic.Microservices.Shared.Data.Entities;
 
 namespace InnowiseClinic.Microservices.Shared.Data.Repositories.Interfaces;
 
-public interface IAsyncRepository<TEntity> where TEntity : Entity
+public interface IRepository<TEntity> where TEntity : Entity
 {
     Task<TEntity?> GetAsync(Guid id);
     Task AddAsync(TEntity entity);
