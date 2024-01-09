b9d6a6141326f2eece88b3b67dab46b282dbd4d3
Windawz <46968683+Windawz@users.noreply.github.com>
2024-01-09 17:06:20 +0300

    Return Task<Guid> from profile data layer's IRepository`1.AddAsync()

--- src/Microservices/Profiles/Data/Repositories/Interfaces/IRepository`1.cs
+++ src/Microservices/Profiles/Data/Repositories/Interfaces/IRepository`1.cs
@@ -5,7 +5,7 @@ namespace InnowiseClinic.Microservices.Profiles.Data.Repositories.Interfaces;
 public interface IRepository<TEntity> where TEntity : IEntity
 {
     Task<TEntity?> GetAsync(Guid id);
-    Task<TEntity> AddAsync(TEntity entity);
+    Task<Guid> AddAsync(TEntity entity);
     Task UpdateAsync(TEntity entity);
     Task DeleteAsync(TEntity entity);
 }
