d3743eca947a8300fa5fb8abfef1744892c14810
Windawz <46968683+Windawz@users.noreply.github.com>
2024-01-11 10:12:37 +0300

    Add doctor repository

---
+++ src/Microservices/Profiles/Data/Repositories/Implementations/DoctorRepository.cs
@@ -0,0 +1,27 @@
+using InnowiseClinic.Microservices.Profiles.Data.Entities.Implementations;
+using InnowiseClinic.Microservices.Profiles.Data.Services.Interfaces;
+
+namespace InnowiseClinic.Microservices.Profiles.Data.Repositories.Implementations;
+
+public class DoctorRepository : Repository<DoctorEntity>
+{
+    public DoctorRepository(IDbConnectionFactory connectionFactory) : base(connectionFactory) { }
+
+    protected override string TableName => "Doctors";
+
+    protected override IEnumerable<(string Key, object? Value)> GetPropertyNamesAndValues(DoctorEntity entity)
+    {
+        return new (string, object?)[]
+        {
+            (nameof(DoctorEntity.AccountId), entity.AccountId),
+            (nameof(DoctorEntity.OfficeId), entity.OfficeId),
+            (nameof(DoctorEntity.SpecializationId), entity.SpecializationId),
+            (nameof(DoctorEntity.FirstName), entity.FirstName),
+            (nameof(DoctorEntity.LastName), entity.LastName),
+            (nameof(DoctorEntity.MiddleName), entity.MiddleName),
+            (nameof(DoctorEntity.DateOfBirth), entity.DateOfBirth),
+            (nameof(DoctorEntity.CareerStartYear), entity.CareerStartYear),
+            (nameof(DoctorEntity.Status), entity.Status),
+        };
+    }
+}
