808d34cc5b5b11c32f6263ef5e18bb4109ae4d10
Windawz <46968683+Windawz@users.noreply.github.com>
2024-01-11 10:25:38 +0300

    Add receptionist repository

---
+++ src/Microservices/Profiles/Data/Repositories/Implementations/ReceptionistRepository.cs
@@ -0,0 +1,23 @@
+using InnowiseClinic.Microservices.Profiles.Data.Entities.Implementations;
+using InnowiseClinic.Microservices.Profiles.Data.Services.Interfaces;
+
+namespace InnowiseClinic.Microservices.Profiles.Data.Repositories.Implementations;
+
+public class ReceptionistRepository : Repository<ReceptionistEntity>
+{
+    public ReceptionistRepository(IDbConnectionFactory connectionFactory) : base(connectionFactory) { }
+
+    protected override string TableName => "Receptionists";
+
+    protected override IEnumerable<(string Key, object? Value)> GetPropertyNamesAndValues(ReceptionistEntity entity)
+    {
+        return new (string, object?)[]
+        {
+            (nameof(ReceptionistEntity.AccountId), entity.AccountId),
+            (nameof(ReceptionistEntity.OfficeId), entity.OfficeId),
+            (nameof(ReceptionistEntity.FirstName), entity.FirstName),
+            (nameof(ReceptionistEntity.LastName), entity.LastName),
+            (nameof(ReceptionistEntity.MiddleName), entity.MiddleName),
+        };
+    }
+}
