1ecc9255a8b271b75396d7385c52e688a5618721
Windawz <46968683+Windawz@users.noreply.github.com>
2024-01-12 16:17:25 +0300

    Add migration runner code

--- src/Microservices/Profiles/MigrationRunner/Program.cs
+++ src/Microservices/Profiles/MigrationRunner/Program.cs
@@ -1,9 +1,46 @@
-﻿namespace InnowiseClinic.Microservices.Profiles.MigrationRunner;
+﻿using FluentMigrator.Runner;
+using InnowiseClinic.Microservices.Profiles.Data.Migrations;
+using InnowiseClinic.Microservices.Profiles.Data.Services.Implementations;
+using InnowiseClinic.Microservices.Profiles.Data.Services.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InnowiseClinic.Microservices.Profiles.MigrationRunner;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .AddCommandLine(args)
+            .Build();
+
+        if (configuration.GetSection("File").Exists())
+        {
+            string filePath = configuration.GetRequiredSection("File").Value!;
+            if (Path.GetExtension(filePath) == ".json" && File.Exists(filePath))
+            {
+                configuration = new ConfigurationBuilder()
+                    .AddConfiguration(configuration)
+                    .AddJsonFile(filePath)
+                    .Build();
+            }
+        }
+
+        using var scope = new ServiceCollection()
+            .AddFluentMigratorCore()
+            .ConfigureRunner(builder => builder
+                .AddPostgres()
+                .WithGlobalConnectionString(configuration.GetConnectionString("Default")
+                    ?? configuration.GetRequiredSection("ConnectionString").Value!)
+                .ScanIn(typeof(Initial).Assembly).For.Migrations())
+            .AddLogging(builder => builder
+                .AddFluentMigratorConsole())
+            .AddSingleton<IEntityMetadataProvider, EntityMetadataProvider>()
+            .BuildServiceProvider(validateScopes: false)
+            .CreateScope();
+
+        scope.ServiceProvider.GetRequiredService<IMigrationRunner>()
+            .MigrateUp();
     }
 }
