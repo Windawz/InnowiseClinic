14ecaacf6557729ca7b01a72616e63cd8f042aee
Windawz <46968683+Windawz@users.noreply.github.com>
2024-01-12 15:38:27 +0300

    Add migration runner console app

---
+++ src/Microservices/Profiles/MigrationRunner/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace InnowiseClinic.Microservices.Profiles.MigrationRunner;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
