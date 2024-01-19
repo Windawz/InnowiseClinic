bedaaf9610ecb7dce17b0316efb04ebe6eb4df51
Windawz <46968683+Windawz@users.noreply.github.com>
2024-01-19 16:41:59 +0300

    Add a patient editing endpoint

--- src/Microservices/Profiles/Api/Controllers/PatientController.cs
+++ src/Microservices/Profiles/Api/Controllers/PatientController.cs
@@ -1,7 +1,9 @@
 using InnowiseClinic.Microservices.Profiles.Api.DataTransferObjects.Requests;
 using InnowiseClinic.Microservices.Profiles.Api.DataTransferObjects.Responses;
+using InnowiseClinic.Microservices.Profiles.Api.DataTransferObjects.Targets;
 using InnowiseClinic.Microservices.Shared.Api.Constants;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace InnowiseClinic.Microservices.Profiles.Api.Controllers;
@@ -20,6 +22,13 @@ public class PatientController : ControllerBase
     [HttpPost]
     [ProducesResponseType(StatusCodes.Status201Created)]
     public IActionResult Create(CreatePatientRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPatch("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public IActionResult Edit(Guid id, JsonPatchDocument<EditPatientTarget> patchDocument)
     {
         throw new NotImplementedException();
     }
