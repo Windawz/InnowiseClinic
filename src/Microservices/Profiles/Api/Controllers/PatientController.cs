98014994fb71483dca05c489df0a44f9f4bdb81c
Windawz <46968683+Windawz@users.noreply.github.com>
2024-01-19 14:58:50 +0300

    Use compile-time string interpolation for patient controller authorize attribute

--- src/Microservices/Profiles/Api/Controllers/PatientController.cs
+++ src/Microservices/Profiles/Api/Controllers/PatientController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace InnowiseClinic.Microservices.Profiles.Api.Controllers;
 
 [ApiController]
-[Authorize(Roles = RoleName.Patient + "," + RoleName.Doctor + "," + RoleName.Receptionist)]
+[Authorize(Roles = $"{RoleName.Patient},{RoleName.Doctor},{RoleName.Receptionist}")]
 public class PatientController : ControllerBase
 {
     [HttpPost]
