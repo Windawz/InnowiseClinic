d8777e218ec8361edf9bba5a455ebf299638e076
Windawz <46968683+Windawz@users.noreply.github.com>
2024-01-24 15:28:47 +0300

    Split full name into parts in GetPatientPageResponse

--- src/Microservices/Profiles/Api/DataTransferObjects/Responses/GetPatientPageResponse.cs
+++ src/Microservices/Profiles/Api/DataTransferObjects/Responses/GetPatientPageResponse.cs
@@ -3,6 +3,8 @@ namespace InnowiseClinic.Microservices.Profiles.Api.DataTransferObjects.Response
 public class GetPatientPageResponse
 {
     public required Guid Id { get; init; }
-    public required string FullName { get; init; }
+    public required string FirstName { get; init; }
+    public required string LastName { get; init; }
+    public string? MiddleName { get; init; }
     public required string PhoneNumber { get; init; }
 }
